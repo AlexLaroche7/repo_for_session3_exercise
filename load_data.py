966ef14401399a46ff79ae6764e374e8ff9ce8e0
Anika Slizewski <a.slizewski@mail.utoronto.ca>
2022-09-16 16:55:45 -0400

    Anika's fancy function addition

--- load_data.py
+++ load_data.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 
 # In this file, you'll put all the functions for creating the Fibonacci sequence up to a certain number.
+ def Fibonacci(n): 
+    if n<= 1:
+        return n
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
